d881274613c03dc5bbdda6ccb1b6a1a6e6416b33
Natalia Barabina <barabash4ka@gmail.com>
2019-04-20 22:01:17 +0300

    2 base-lesson

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,83 @@
-'use strict'
+function main() {
+    "use strict";
+    
+    let money = +prompt('Ваш бюджет на месяц?', ''),
+        time = prompt('Введите дату в формате YYYY-MM-DD', '');
+    
+    let appData = {
+        moneyDate: money,
+        timeData: time,
+        expenses: {},
+        optionalExpenses: {},
+        income: [],
+        savings: false   
+    };   
+    
+    for (let i = 0; i < 2; i++){
 
-let money = +prompt('Ваш бюджет на месяц?', ''),
-    time = prompt('Введите дату в формате YYYY-MM-DD', ''),
-    requiredState = prompt('Введите обязательную статью расходов в этом месяце', ''),
-    howMuch = prompt('Во сколько обойдется?', '');
+        let requiredState = prompt('Введите обязательную статью расходов в этом месяце', ''),
+            howMuch = prompt('Во сколько обойдется?', '');
 
-let appData = {
-    moneyDate: money,
-    timeData: time,
-    expenses: {},
-    optionalExpenses: {},
-    income: [],
-    savings: false      
-};   
+        if ( typeof(requiredState) === 'string' && requiredState != null && requiredState != '' && requiredState.length < 50 &&
+            howMuch != null && howMuch != '') {
 
-appData.expenses[requiredState] = howMuch;
+            console.log('is worked');
+            appData.expenses[requiredState] = howMuch;
+        } else {
+            i--;
+        }
+    }
 
-requiredState = prompt('Введите обязательную статью расходов в этом месяце', ''),
-howMuch = prompt('Во сколько обойдется?', '');
+    // let i = 0;
 
-appData.expenses[requiredState] = howMuch;
+    // while (i < 2){
+    //     i++;
 
-alert('Бютжет на 1 день - ' + '' + appData.moneyDate/30);
+    //     let requiredState = prompt('Введите обязательную статью расходов в этом месяце', ''),
+    //         howMuch = prompt('Во сколько обойдется?', '');
 
-console.log(appData); 
+    //     if ( typeof(requiredState) === 'string' && requiredState != null && requiredState != '' && requiredState.length < 50
+    //          && howMuch != null && howMuch != '') {
+
+    //         console.log('is worked');
+    //         appData.expenses[requiredState] = howMuch;
+    //     } else {
+    //         i--;
+    //     }
+    // }
+
+    // let i = 0;
+
+    // do {
+    //     i++;
+
+    //     let requiredState = prompt('Введите обязательную статью расходов в этом месяце', ''),
+    //         howMuch = prompt('Во сколько обойдется?', '');
+
+    //     if ( typeof(requiredState) === 'string' && requiredState != null && requiredState != '' && requiredState.length < 50
+    //          && howMuch != null && howMuch != '') {
+
+    //         console.log('is worked');
+    //         appData.expenses[requiredState] = howMuch;
+    //     } else {
+    //         i--;
+    //     }
+    // } while (i < 2);
+
+    appData.dailyMoney = appData.moneyDate/30;
+
+    alert('Бютжет на 1 день - ' + '' + appData.moneyDate/30);
+
+    if (appData.dailyMoney < 1000) {
+        console.log('малый уровень дохода');  
+    } else if (appData.dailyMoney > 1000 && appData.dailyMoney < 2500){
+        console.log('средний уровень дохода'); 
+    } else if (appData.dailyMoney > 2500){
+        console.log('высокий уровень дохода'); 
+    } else {
+        console.log('ошибка'); 
+    }
+
+    console.log(appData);  
+}
+main();
---
+++ src/js/tempCodeRunnerFile.js
@@ -0,0 +1,16 @@
+    var something = 100;
+
+    switch (something){
+        case (something < 1000):
+             console.log('маленький уровень дохода'); 
+             break;
+        case (something > 2500):
+             console.log('средний уровень дохода'); 
+             break;
+        case (something > 2500):
+             console.log('высокий уровень дохода'); 
+             break;
+        default:
+            console.log('что-то пошло не так :('); 
+            break; 
+    }
