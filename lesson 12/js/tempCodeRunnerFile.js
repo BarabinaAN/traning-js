4d8ca90874f6978289dfd275a9bc1c5188eedd67
Natalia Barabina <barabash4ka@gmail.com>
2019-05-11 11:15:57 +0300

    12 base lesson

---
+++ lesson 12/js/tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+            let deffValue = getTimeDifference(endtime),
+                size = Object.keys(deffValue).length;
+
+            for (let i = 1; i < size; i++){
+                i;
+                // if (deffValue[i] < 10) {
+                //     deffValue[i] = '0' + deffValue[i];
+                // }
+            }
+            console.log(size);
